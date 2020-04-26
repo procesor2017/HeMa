4b098e4dd7c1e24341792b139d22f7fce81aa7e1
Jan Egermaier <jan.egermaier@tesena.com>
2020-04-26 20:30:44 +0200

    GeneratingMap

--- modul/GenerateMap.py
+++ modul/GenerateMap.py
@@ -78,7 +78,7 @@ class CreateWorld:
             if world[CoorCityListX[e], CoorCityListY[e]] < -0.05:
                 pass
             elif world[CoorCityListX[e], CoorCityListY[e]] < 0.15:
-                coin = random.randrange(0, 3)
+                coin = random.randrange(0, 5)
                 if coin == 1:
                     color_world[CoorCityListX[e], CoorCityListY[e]] = color_City_elf
                     RealCityCoor = []
@@ -89,6 +89,7 @@ class CreateWorld:
                     RealCityCoor.append(10)                             # power of one people
                     RealCityCoor.append(random.randrange(50, 100))                   # Power of civi
                     RealCityCoor.append(random.randrange(1, 10))
+                    RealCityCoor.append('elf')
                     self.PowerofAllElfs.append(RealCityCoor)
                 else:
                     color_world[CoorCityListX[e], CoorCityListY[e]] = color_City_human
@@ -100,6 +101,7 @@ class CreateWorld:
                     RealCityCoor.append(1)                             # power of one people
                     RealCityCoor.append(random.randrange(0, 50))                   # Power of civi
                     RealCityCoor.append(random.randrange(3, 7))
+                    RealCityCoor.append('human')
                     self.PowerofAllHuman.append(RealCityCoor)
 
             elif world[CoorCityListX[e], CoorCityListY[e]] < 0.22:
@@ -112,6 +114,7 @@ class CreateWorld:
                 RealCityCoor.append(5)  # power of one people
                 RealCityCoor.append(random.randrange(25, 75))  # Power of civi
                 RealCityCoor.append(random.randrange(3, 7))
+                RealCityCoor.append('dwarf')
                 self.PowerofAllDwarf.append(RealCityCoor)
             else:
                 pass
@@ -120,76 +123,328 @@ class CreateWorld:
 
     def create_history1(self):
 
-        DistanceCity = 50
-        LenghtPower = len(self.PowerofAllDwarf)
+        DistanceCity = 50 # random.randrange(250, 500)
+        LenghtPowerDwarf = len(self.PowerofAllDwarf)
+        LenghtPowerElf = len(self.PowerofAllElfs)
+        LenghtPowerHuman = len(self.PowerofAllHuman)
         i = 0
-        while i < LenghtPower:
+        while i < LenghtPowerDwarf:
             y1 = int(self.PowerofAllDwarf[i][1])
             x1 = int(self.PowerofAllDwarf[i][2])
             fk = 1
-            while fk < LenghtPower:
+            u = 0
+            while fk < LenghtPowerDwarf:
                 y = int(self.PowerofAllDwarf[fk][1])
                 x = int(self.PowerofAllDwarf[fk][2])
                 w = 0
-                u = 0
                 if (x1 - DistanceCity) < int(x) < (x1 + DistanceCity) and (y1 - DistanceCity) < int(y) < (
                         y1 + DistanceCity):
-                    if (y1 - DistanceCity) < int(y) < (y1 + DistanceCity):
-                        # (početobyvatel * 0,7) * síla_jednoho bojovníka * (sílacivilizace /100 )
-                        Nation1 = (int(self.PowerofAllDwarf[i][3]) * 0.75) * int(self.PowerofAllDwarf[i][4]) * (int(self.PowerofAllDwarf[i][5]) / 100)
-                        Nation2 = (int(self.PowerofAllDwarf[fk][3]) * 0.75) * int(self.PowerofAllDwarf[fk][4]) * (int(self.PowerofAllDwarf[fk][5]) / 100)
-                        Nation1List = self.PowerofAllDwarf[i]
-                        Nation2List = self.PowerofAllDwarf[fk]
-                        with open('../data/HistoryOfDwarf.csv', 'a') as history:
-                            if Nation1 < Nation2:
-                                print(y, x, y1, x1)
-                                for x in self.PowerofAllDwarf:
-                                    if x == Nation1List:
-                                        self.PowerofAllDwarf.remove(x)
-                                        history.write(
-                                            'Dwarf clan {WeakClan} was destroyed by {Strongclan} in {age} \n'.format(
-                                                WeakClan=Nation1List[0], Strongclan=Nation2List[0],
-                                                age=random.randrange(0, 1000)))
-                                        w = 1
-                                        u = 1
-                            elif Nation1 == Nation2:
-                                pass
-                            else:
-                                for x in self.PowerofAllDwarf:
-                                    if x == Nation2List:
-                                        self.PowerofAllDwarf.remove(x)
-                                        history.write(
-                                            'Dwarf clan {WeakClan} was destroyed by {Strongclan} in {age} \n'.format(
-                                                WeakClan=Nation1List[0], Strongclan=Nation2List[0],
-                                                age=random.randrange(0, 1000)))
-                                        w = 1
-                                        u = 1
+                    # (početobyvatel * 0,7) * síla_jednoho bojovníka * (sílacivilizace /100 )
+                    Nation1 = (int(self.PowerofAllDwarf[i][3]) * 0.75) * int(self.PowerofAllDwarf[i][4]) * (int(self.PowerofAllDwarf[i][5]) / 100)
+                    Nation2 = (int(self.PowerofAllDwarf[fk][3]) * 0.75) * int(self.PowerofAllDwarf[fk][4]) * (int(self.PowerofAllDwarf[fk][5]) / 100)
+                    Nation1List = self.PowerofAllDwarf[i]
+                    Nation2List = self.PowerofAllDwarf[fk]
+                    with open('../data/HistoryOfDwarf.csv', 'a') as history:
+                        if Nation1 < Nation2:
+                            for x in self.PowerofAllDwarf:
+                                if x == Nation1List:
+                                    self.PowerofAllDwarf.remove(x)
+                                    history.write(
+                                        'Dwarf clan {WeakClan} was destroyed by {Strongclan} in {age} \n'.format(
+                                            WeakClan=Nation1List[0], Strongclan=Nation2List[0],
+                                            age=random.randrange(0, 5000)))
+                                    u = 1
+                        elif Nation1 == Nation2:
+                            pass
+                        else:
+                            for x in self.PowerofAllDwarf:
+                                if x == Nation2List:
+                                    self.PowerofAllDwarf.remove(x)
+                                    history.write(
+                                        'Dwarf clan {WeakClan} was destroyed by {Strongclan} in {age} \n'.format(
+                                            WeakClan=Nation2List[0], Strongclan=Nation1List[0],
+                                            age=random.randrange(0, 5000)))
+                                    w = 1
+                else:
+                    self.PowerofAllDwarf[i][3] = self.PowerofAllDwarf[i][3] * 1.5
+                    self.PowerofAllDwarf[i][5] = self.PowerofAllDwarf[i][5] * 1.2
                 if w == 1:
                     pass
                 else:
                     fk += 1
-                LenghtPower = len(self.PowerofAllDwarf)
+                LenghtPowerDwarf = len(self.PowerofAllDwarf)
             if u == 1:
                 pass
             else:
                 i += 1
-            LenghtPower = len(self.PowerofAllDwarf)
+            LenghtPowerDwarf = len(self.PowerofAllDwarf)
+
+        i = 0
+        while i < LenghtPowerElf:
+            y1 = int(self.PowerofAllElfs[i][1])
+            x1 = int(self.PowerofAllElfs[i][2])
+            fk = 1
+            u = 0
+            while fk < LenghtPowerElf:
+                y = int(self.PowerofAllElfs[fk][1])
+                x = int(self.PowerofAllElfs[fk][2])
+                w = 0
+                if (x1 - DistanceCity) < int(x) < (x1 + DistanceCity) and (y1 - DistanceCity) < int(y) < (
+                        y1 + DistanceCity):
+                    # (početobyvatel * 0,7) * síla_jednoho bojovníka * (sílacivilizace /100 )
+                    Nation1 = (int(self.PowerofAllElfs[i][3]) * 0.75) * int(self.PowerofAllElfs[i][4]) * (int(self.PowerofAllElfs[i][5]) / 100)
+                    Nation2 = (int(self.PowerofAllElfs[fk][3]) * 0.75) * int(self.PowerofAllElfs[fk][4]) * (int(self.PowerofAllElfs[fk][5]) / 100)
+                    Nation1List = self.PowerofAllElfs[i]
+                    Nation2List = self.PowerofAllElfs[fk]
+                    with open('../data/HistoryOfElf.csv', 'a') as history:
+                        if Nation1 < Nation2:
+                            for x in self.PowerofAllElfs:
+                                if x == Nation1List:
+                                    self.PowerofAllElfs.remove(x)
+                                    history.write(
+                                        'Elf house {WeakClan} was destroyed by {Strongclan} in {age} \n'.format(
+                                            WeakClan=Nation1List[0], Strongclan=Nation2List[0],
+                                            age=random.randrange(0, 5000)))
+                                    u = 1
+                        elif Nation1 == Nation2:
+                            pass
+                        else:
+                            for x in self.PowerofAllElfs:
+                                if x == Nation2List:
+                                    self.PowerofAllElfs.remove(x)
+                                    history.write(
+                                        'Elf house {WeakClan} was destroyed by {Strongclan} in {age} \n'.format(
+                                            WeakClan=Nation2List[0], Strongclan=Nation1List[0],
+                                            age=random.randrange(0, 5000)))
+                                    w = 1
+                else:
+                    self.PowerofAllElfs[i][3] = self.PowerofAllElfs[i][3] * 1.5
+                    self.PowerofAllElfs[i][5] = self.PowerofAllElfs[i][5] * 1.2
+
+                if w == 1:
+                    pass
+                else:
+                    fk += 1
+                LenghtPowerElf = len(self.PowerofAllElfs)
+            if u == 1:
+                pass
+            else:
+                i += 1
+            LenghtPowerElf = len(self.PowerofAllElfs)
+
+        i = 0
+        while i < LenghtPowerHuman:
+            y1 = int(self.PowerofAllHuman[i][1])
+            x1 = int(self.PowerofAllHuman[i][2])
+            fk = 1
+            u = 0
+            while fk < LenghtPowerHuman:
+                y = int(self.PowerofAllHuman[fk][1])
+                x = int(self.PowerofAllHuman[fk][2])
+                w = 0
+                if (x1 - DistanceCity) < int(x) < (x1 + DistanceCity) and (y1 - DistanceCity) < int(y) < (
+                        y1 + DistanceCity):
+                    # (početobyvatel * 0,7) * síla_jednoho bojovníka * (sílacivilizace /100 )
+                    Nation1 = (int(self.PowerofAllHuman[i][3]) * 0.75) * int(self.PowerofAllHuman[i][4]) * (
+                                int(self.PowerofAllHuman[i][5]) / 100)
+                    Nation2 = (int(self.PowerofAllHuman[fk][3]) * 0.75) * int(self.PowerofAllHuman[fk][4]) * (
+                                int(self.PowerofAllHuman[fk][5]) / 100)
+                    Nation1List = self.PowerofAllHuman[i]
+                    Nation2List = self.PowerofAllHuman[fk]
+                    with open('../data/HistoryOfDwarf.csv', 'a') as history:
+                        if Nation1 < Nation2:
+                            for x in self.PowerofAllHuman:
+                                if x == Nation1List:
+                                    self.PowerofAllHuman.remove(x)
+                                    history.write(
+                                        'Dwarf clan {WeakClan} was destroyed by {Strongclan} in {age} \n'.format(
+                                            WeakClan=Nation1List[0], Strongclan=Nation2List[0],
+                                            age=random.randrange(0, 5000)))
+                                    u = 1
+                        elif Nation1 == Nation2:
+                            pass
+                        else:
+                            for x in self.PowerofAllHuman:
+                                if x == Nation2List:
+                                    self.PowerofAllHuman.remove(x)
+                                    history.write(
+                                        'Dwarf clan {WeakClan} was destroyed by {Strongclan} in {age} \n'.format(
+                                            WeakClan=Nation2List[0], Strongclan=Nation1List[0],
+                                            age=random.randrange(0, 5000)))
+                                    w = 1
+                else:
+                    self.PowerofAllHuman[i][3] = self.PowerofAllHuman[i][3] * 1.5
+                    self.PowerofAllHuman[i][5] = self.PowerofAllHuman[i][5] * 1.2
+                if w == 1:
+                    pass
+                else:
+                    fk += 1
+                LenghtPowerHuman = len(self.PowerofAllHuman)
+            if u == 1:
+                pass
+            else:
+                i += 1
+            LenghtPowerHuman = len(self.PowerofAllHuman)
+
         DwarfCity = self.PowerofAllDwarf
-        print(DwarfCity)
-        print('start open oldmap')
+        ElfCity = self.PowerofAllElfs
+        HumanCity = self.PowerofAllHuman
+
+        print('Počet elfu:'+ str(len(ElfCity)))
+        print('Počet dwarfu:' + str(len(DwarfCity)))
+        print('Počet humanu:' + str(len(HumanCity)))
+
         OldMap = Image.open('../data/map.png')
         e = 0
-        print(len(DwarfCity))
+
+        while e < len(ElfCity):
+            draw = ImageDraw.Draw(OldMap)
+            y1 = int(ElfCity[e][1])
+            x1 = int(ElfCity[e][2])
+            shape = [(x1, y1), (x1+10, y1+10)]
+            draw.rectangle(shape, fill="purple")
+            OldMap.save('../data/map2.png', 'PNG')
+            e += 1
+
+        OldMap = Image.open('../data/map2.png')
+        e = 0
         while e < len(DwarfCity):
-            print('start print')
             draw = ImageDraw.Draw(OldMap)
             y1 = int(DwarfCity[e][1])
             x1 = int(DwarfCity[e][2])
             shape = [(x1, y1), (x1+10, y1+10)]
-            draw.rectangle(shape, fill="pink")
+            draw.rectangle(shape, fill="black")
             OldMap.save('../data/map2.png', 'PNG')
             e += 1
-        print('end?')
+
+        OldMap = Image.open('../data/map2.png')
+        e = 0
+        while e < len(HumanCity):
+            draw = ImageDraw.Draw(OldMap)
+            y1 = int(HumanCity[e][1])
+            x1 = int(HumanCity[e][2])
+            shape = [(x1, y1), (x1+10, y1+10)]
+            draw.rectangle(shape, fill="red")
+            OldMap.save('../data/map2.png', 'PNG')
+            e += 1
+        # Druhý věk (Národy se řežou mezi sebou)
+        ListAllRaces = []
+        for x in DwarfCity:
+            ListAllRaces.append(x)
+        for x in ElfCity:
+            ListAllRaces.append(x)
+        for x in HumanCity:
+            ListAllRaces.append(x)
+
+        LenListAllRaces = len(ListAllRaces)
+        DistanceCity2 = 120
+        i = 0
+        while i < LenListAllRaces:
+            y1 = int(ListAllRaces[i][1])
+            x1 = int(ListAllRaces[i][2])
+            fk = 1
+            u = 0
+            while fk < LenListAllRaces:
+                y = int(ListAllRaces[fk][1])
+                x = int(ListAllRaces[fk][2])
+                w = 0
+                if (x1 - DistanceCity2) < int(x) < (x1 + DistanceCity2) and (y1 - DistanceCity2) < int(y) < (
+                        y1 + DistanceCity2):
+                    # (početobyvatel * 0,7) * síla_jednoho bojovníka * (sílacivilizace /100 )
+                    Nation1 = (int(ListAllRaces[i][3]) * 0.75) * int(ListAllRaces[i][4]) * (
+                                int(ListAllRaces[i][5]) / 100)
+                    Nation2 = (int(ListAllRaces[fk][3]) * 0.75) * int(ListAllRaces[fk][4]) * (
+                                int(ListAllRaces[fk][5]) / 100)
+                    Nation1List = ListAllRaces[i]
+                    Nation2List = ListAllRaces[fk]
+                    with open('../data/HistoryOfAll.csv', 'a') as history:
+                        if Nation1 < Nation2:
+                            for x in ListAllRaces:
+                                if x == Nation1List:
+                                    ListAllRaces.remove(x)
+                                    history.write(
+                                        ' {WeakClan} was destroyed by {Strongclan} in {age} \n'.format(
+                                            WeakClan=Nation1List[0], Strongclan=Nation2List[0],
+                                            age=random.randrange(0, 5000)))
+                                    u = 1
+                        elif Nation1 == Nation2:
+                            pass
+                        else:
+                            for x in ListAllRaces:
+                                if x == Nation2List:
+                                    ListAllRaces.remove(x)
+                                    history.write(
+                                        '{WeakClan} was destroyed by {Strongclan} in {age} \n'.format(
+                                            WeakClan=Nation2List[0], Strongclan=Nation1List[0],
+                                            age=random.randrange(0, 5000)))
+                                    w = 1
+                else:
+                    ListAllRaces[i][3] = ListAllRaces[i][3] * 1.5
+                    ListAllRaces[i][5] = ListAllRaces[i][5] * 1.2
+
+                if w == 1:
+                    pass
+                else:
+                    fk += 1
+                LenListAllRaces = len(ListAllRaces)
+            if u == 1:
+                pass
+            else:
+                i += 1
+            LenListAllRaces = len(ListAllRaces)
+        # list nation after second age
+        Elfa2a =[]
+        Dwarf2a = []
+        Human2a = []
+
+        for x in ListAllRaces:
+            if x[7] == 'elf':
+                Elfa2a.append(x)
+            elif x[7] == 'dwarf':
+                Dwarf2a.append(x)
+            elif x[7] == 'human':
+                Human2a.append(x)
+
+        print('Počet elfu:'+ str(len(Elfa2a)))
+        print('Počet dwarfu:' + str(len(Dwarf2a)))
+        print('Počet humanu:' + str(len(Human2a)))
+
+        OldMap = Image.open('../data/map1.png')
+        e = 0
+        while e < len(Elfa2a):
+            draw = ImageDraw.Draw(OldMap)
+            y1 = int(Elfa2a[e][1])
+            x1 = int(Elfa2a[e][2])
+            shape = [(x1, y1), (x1+10, y1+10)]
+            draw.rectangle(shape, fill="purple")
+            OldMap.save('../data/map3.png', 'PNG')
+            e += 1
+
+        OldMap = Image.open('../data/map3.png')
+        e = 0
+        while e < len(Dwarf2a):
+            draw = ImageDraw.Draw(OldMap)
+            y1 = int(Dwarf2a[e][1])
+            x1 = int(Dwarf2a[e][2])
+            shape = [(x1, y1), (x1+10, y1+10)]
+            draw.rectangle(shape, fill="black")
+            OldMap.save('../data/map3.png', 'PNG')
+            e += 1
+
+        OldMap = Image.open('../data/map3.png')
+        e = 0
+        while e < len(Human2a):
+            draw = ImageDraw.Draw(OldMap)
+            y1 = int(Human2a[e][1])
+            x1 = int(Human2a[e][2])
+            shape = [(x1, y1), (x1+10, y1+10)]
+            draw.rectangle(shape, fill="red")
+            OldMap.save('../data/map3.png', 'PNG')
+            e += 1
+
+
+
+
 
 
 create_world = CreateWorld()
